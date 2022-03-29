301d0f39a28635fe2dfac8e951db70e69085939a
Zion Yang <zionremnant@gmail.com>
2022-03-28 18:28:22 -0700

    middleware server.js

--- server.js
+++ server.js
@@ -1,37 +1,21 @@
 const express = require("express");
+const res = require("express/lib/response");
+const fs = require("fs");
 const path = require("path");
-const { clog } = require("./middleware/clog");
-const api = require("./routes/index.js");
-
+const notes = require("./db/db.json");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Import custom middleware, "cLog"
-app.use(clog);
-
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", api);
-
 app.use(express.static("public"));
 
-// GET Route for homepage
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
+app.use("/api/notes", (req, res) => {
+  res.json(notes.slice(1));
+});
 
-// GET Route for feedback page
-app.get("/feedback", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/pages/feedback.html"))
-);
-
-// Wildcard route to direct users to a 404 page
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/pages/404.html"))
-);
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.listen(PORT, () => {
+  console.log(`App listening at ${PORT} 🚀`);
+});
