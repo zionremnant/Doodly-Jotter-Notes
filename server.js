30ac801eab83fcdf4bac99401d29e256a0716597
Zion Yang <zionremnant@gmail.com>
2022-03-29 20:19:42 -0700

    switch to function

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const allNotes = require("./db/db.json");
-const PORT = process.env.PORT || 3001;
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
@@ -11,63 +10,97 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use("/api/notes", (req, res) => {
-  res.json(allNotes.slice(1));
-});
-
 // GET Route for homepage
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 // GET Route for notes page
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-// Wildcard route to direct users to a 404 page
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-function createNew(body, notesArr) {
-  const newNote = body;
-  if (!Array.isArray(notesArr)) notesArr = [];
-  if (notesArr.length === 0) notesArr.push(0);
-  body.id = notesArr[0];
-  notesArr[0]++;
-  notesArr.push(newNote);
-  fs.writeFileSync(
-    path.join(__dirname, "/db/db.json"),
-    JSON.stringify(notesArr, null, 2)
-  );
-  return newNote;
-}
-
-app.post("api/notes", (req, res) => {
-  const newNote = createNew(req.body, allNotes);
-  res.json(newNote);
-});
-
-function deleteNote(id, notesArr) {
-  for (let i = 0; i < notesArr.length; i++) {
-    let note = notesArr[i];
-    if (note.id == id) {
-      notesArr.splice(i, 1);
-      fs.writeFileSync(
-        path.join(__dirname, "/db/db.json"),
-        JSON.stringify(notesArr, null, 2)
-      );
-      break;
+app.post("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", "utf8", function (error, notes) {
+    if (error) {
+      return console.log(error);
     }
-  }
-}
+    notes = JSON.parse(notes);
 
-app.delete("/api/notes/:id", (req, res) => {
-  deleteNote(req.params.id, allNotes);
-  res.json(true);
+    var id = notes[notes.length - 1].id + 1;
+    var newNote = { title: req.body.title, text: req.body.text, id: id };
+    var activeNote = notes.concat(newNote);
+
+    fs.writeFile(
+      __dirname + "/db/db.json",
+      JSON.stringify(activeNote),
+      function (error, data) {
+        if (error) {
+          return error;
+        }
+        console.log(activeNote);
+        req.json(activeNote);
+      }
+    );
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening at ${PORT} 🚀`);
+// pull from db.json
+app.get("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", "utf8", function (error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("This is Note", data);
+    res.json(JSON.parse(data));
+  });
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  const noteId = JSON.parse(req.params.id);
+  console.log(noteId);
+  fs.readFile(__dirname + "/db/db.json", "utf8", function (error, notes) {
+    if (error) {
+      return console.log(error);
+    }
+    notes = JSON.parse(notes);
+    notes = notes.filter((val) => val.id !== noteId);
+    fs.writeFile(
+      __dirname + "/db/db.json",
+      JSON.stringify(notes),
+      function (error, data) {
+        if (error) {
+          return error;
+        }
+        res.json(notes);
+      }
+    );
+  });
+});
+
+app.put("/api/notes/:id", function (req, res) {
+  const noteId = JSON.parse(req.params, id);
+  console.log(noteId);
+  fs.readFile(__dirname + "db/db.json", "utf8", function (error, notes) {
+    if (error) {
+      return console.log(error);
+    }
+    notes.JSON.parse(notes);
+    notes = notes.filter((val) => val.id !== noteId);
+    fs.writeFile(
+      __dirname + "db/db.json",
+      JSON.stringify(notes),
+      function (error, data) {
+        if (error) {
+          return error;
+        }
+        res.json(notes);
+      }
+    );
+  });
+});
+
+// starts the server to begin listening
+app.listen(PORT, function () {
+  console.log("App listening at PORT 🚀" + PORT);
 });
