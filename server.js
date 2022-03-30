8c7e617bab02c220185e7c53d80d28bf93fb18b0
Zion Yang <zionremnant@gmail.com>
2022-03-29 18:31:08 -0700

    minor fixes server.js

--- server.js
+++ server.js
@@ -17,17 +17,17 @@ app.use("/api/notes", (req, res) => {
 
 // GET Route for homepage
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // GET Route for notes page
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 // Wildcard route to direct users to a 404 page
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 function createNew(body, notesArr) {
